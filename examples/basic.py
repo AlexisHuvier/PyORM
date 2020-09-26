33ef3b19e279307b57b0420d33d72111e5b2398b
LavaPower <lavapower84@gmail.com>
2020-09-26 12:50:22 +0200

    Update Example

--- examples/basic.py
+++ examples/basic.py
@@ -1,15 +1,20 @@
 from pyorm import Table, Database, Column
 from pyorm.utils import ValueType
 
-class Person(Table):
-    id = Column(type_=ValueType.INT, primary_key=True, unique=True, auto_increment=True, not_null=True)
-    name = Column(type_=ValueType.VARCHAR, not_null=True)
-    db = Database("module=sqlite;db=base.db")
+import os
 
-michel = Person(id=1, name="Michel")
-print(michel.id)
-print(michel.name)
+class Person(Table):
+    columns = {
+        "id": Column("id", type_=ValueType.INT, primary_key=True, auto_increment=True),
+        "name": Column("name", type_=ValueType.VARCHAR, not_null=True)
+    }
+    db = Database("module=sqlite;db="+os.path.dirname(__file__)+"/base.db")
+
+Person.create()
+Person.delete_all()
+print(Person.get_all())
+michel = Person(name="Michel")
 print(michel)
-michel.id = 4
-print(michel.id)
-print(michel)
+michel.save()
+for i in Person.get_all():
+    print(i)
