0cf384588280a187c2f2844307cac9e186530237
LavaPower <lavapower84@gmail.com>
2020-09-26 12:49:56 +0200

    Create sql description of column

--- pyorm/column.py
+++ pyorm/column.py
@@ -3,10 +3,31 @@ from pyorm.utils import ValueType
 __all__ = ["Column"]
 
 class Column:
-    def __init__(self, type_=ValueType.INT, primary_key=False, not_null = False, unique = False, auto_increment = False, default = None):
+    def __init__(self, name, type_=ValueType.INT, primary_key=False, not_null = False, unique = False, auto_increment = False, default = None):
+        self.name = name
         self.type_ = type_
         self.primary_key = primary_key
         self.not_null = not_null
-        self.default = default
         self.unique = unique
         self.auto_increment = auto_increment
+        self.default = default
+    
+    def get_sql_description(self):
+        parts = [self.name, self.type_]
+        if self.primary_key:
+            parts.append("PRIMARY KEY")
+        if self.auto_increment:
+            parts.append("AUTOINCREMENT")
+        if self.not_null:
+            parts.append("NOT NULL")
+        if self.unique:
+            parts.append("UNIQUE")
+        if self.default is not None:
+            parts.append("DEFAULT "+str(self.default))
+        
+        return " ".join(parts)
+    
+    def __str__(self):
+        return self.name+"(type = {}, primary_key = {}, not_null = {}, unique = {}, auto_increment = {}, default = {})".format(
+            self.type_, self.primary_key, self.not_null, self.unique, self.auto_increment, self.default
+        )
